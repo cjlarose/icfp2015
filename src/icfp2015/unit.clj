2c4911d58f7ac2b4c4beb9f58d245e26817bf1f1
Roey D. Chasman <rchasman@gmail.com>
2015-08-08 15:44:45 -0700

    RDC - Add enumerations of rotation directions.

--- src/icfp2015/unit.clj
+++ src/icfp2015/unit.clj
@@ -8,8 +8,45 @@
         :pivot (f (:pivot unit)) })))
 
 (def translations
-  { :west [-1 0]
+  { :north [0 -2]
+    :west [-1 0]
     :east [1 0]
+    :south [0 2]
     :southwest [0 1]
-    :southeast [1 1] })
+    :southeast [1 1]
+})
 
+(defn rotate
+  "Rotate a unit around a pivot point"
+  [rotation, {:keys pi pj :as pivot}]
+  nil)
+
+(def rotations
+  [:counter-clockwise
+    { :origin [0 0]
+      :northwest :southwest
+      :northwest-offset :south
+      :north :southwest-offset
+      :west :southeast
+      :southwest :east
+      :southwest-offset :southeast-offset
+      :southeast :northeast
+      :south :northeast-offset
+      :southeast-offset :north
+      :east :northwest
+      :northeast :west
+      :northeast-offset :northwest-offset }
+  :clockwise
+    { :origin [0 0]
+      :northwest :east
+      :northwest-offset :northeast-offset
+      :north :southeast-offset
+      :west :southwest
+      :southwest :northwest
+      :southwest-offset :north
+      :southeast :west
+      :south :northwest-offset
+      :southeast-offset :southwest-offset
+      :east :southwest
+      :northeast :southeast
+      :northeast-offset :south }])
