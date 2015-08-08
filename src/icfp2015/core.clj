e39d2588a1ae68a86e888944dfbde0340835391e
Roey D. Chasman <rchasman@gmail.com>
2015-08-08 00:10:05 -0700

    RDC - Load function for JSON parsing.

--- src/icfp2015/core.clj
+++ src/icfp2015/core.clj
@@ -1,5 +1,11 @@
 (ns icfp2015.core
+  (:require [clojure.data.json :as json])
   (:gen-class))
+
+(defn load-json
+  "Parse JSON problems"
+  [json]
+  (json/read-str json :key-fn keyword))
 
 (defn -main
   "I don't do a whole lot ... yet."
