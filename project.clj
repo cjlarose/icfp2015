4397e0a20fa866235479eadf46bc9256f79df807
Chris LaRose <chris.larose@mycase.com>
2015-08-08 09:15:23 -0700

    cjl - Add command line parsing lib

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.cli "0.3.2"]]
   :main ^:skip-aot icfp2015.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
