4f529f4788364412646380a8c7d7e7171afc6a6f
Chris LaRose <chris.larose@mycase.com>
2015-08-07 23:45:01 -0700

    cjl - Begin test for get-games

--- test/icfp2015/problem_test.clj
+++ test/icfp2015/problem_test.clj
@@ -10,3 +10,21 @@
 (deftest source-seq-test
   (testing "Correct sequence"
     (is (= [0 24107 16552 12125 9427 13152 21440 3383 6873 16117] (take 10 (source-sequence 17))))))
+
+(deftest get-games-test
+  (testing "gets correct games"
+    (let [problem { :width 10
+                    :height 15
+                    :sourceSeeds [ 0 17 5235 ]
+                    :sourceLength 5
+                    :filled [ { :x 5 :y 4 } { :x 6 :y 2 } ]
+                    :units [ { :members [ { :x 1 :y 2 } { :x 0 :y 5 } ]
+                               :pivot { :x 0 :y 0 } } 
+                             { :members [ { :x 0 :y 1 } { :x 5 :y 2 } ]
+                               :pivot { :x 0 :y 1 } } 
+                             { :members [ { :x 11 :y 6 } { :x 1 :y 4 } ]
+                               :pivot { :x 6 :y 1 } } 
+                             { :members [ { :x 4 :y 7 } { :x 11 :y 8 } ]
+                               :pivot { :x 4 :y 2 } } ] }
+          games (get-games problem)]
+      (is (= 2 (+ 1 1))))))
