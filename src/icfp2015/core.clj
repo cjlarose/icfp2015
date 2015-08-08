f0553507710d26b3077fa9e9a3fe22b220669563
Chris LaRose <chris.larose@mycase.com>
2015-08-07 18:21:38 -0700

    cjl - Generating correct source sequence

--- src/icfp2015/core.clj
+++ src/icfp2015/core.clj
@@ -6,6 +6,11 @@
     (let [next-item (fn [previous-item] (mod (+ (* multiplier previous-item) increment) modulus))]
       (iterate next-item seed))))
 
+(defn source-sequence [seed]
+  (let [raw-seq (lcg (bit-shift-left 1 32) 1103515245 12345)
+        truncate-bits (fn [x] (bit-and (bit-shift-right x 16) 0x7FFF))]
+    (map truncate-bits (raw-seq seed))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/icfp2015/core_test.clj
+++ test/icfp2015/core_test.clj
@@ -6,3 +6,7 @@
   (testing "Correct sequence"
     (let [f (lcg (bit-shift-left 1 32) 1103515245 12345)]
       (is (= [17 1579902326 3232257911 794669028 2765327437] (take 5 (f 17)))))))
+
+(deftest source-seq-test
+  (testing "Correct sequence"
+    (is (= [0 24107 16552 12125 9427 13152 21440 3383 6873 16117] (take 10 (source-sequence 17))))))
