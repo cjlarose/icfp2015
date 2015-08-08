e09dbdc105eaf8f07980026d3193290c8f66ce63
Chris LaRose <chris.larose@mycase.com>
2015-08-07 22:25:46 -0700

    cjl - Add problem test file

---
+++ test/icfp2015/problem_test.clj
@@ -0,0 +1,7 @@
+(ns icfp2015.problem-test
+  (:require [clojure.test :refer :all]
+            [icfp2015.problem :refer :all]))
+
+(deftest get-games-test
+  (testing "Gets games"
+    (is (= 1 1))))
