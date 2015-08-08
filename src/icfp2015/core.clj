e282ee20ce475b5e02f07d974c5413f736fed858
Chris LaRose <chris.larose@mycase.com>
2015-08-07 18:09:41 -0700

    cjl - Add linear congruential generator fn

--- src/icfp2015/core.clj
+++ src/icfp2015/core.clj
@@ -1,6 +1,11 @@
 (ns icfp2015.core
   (:gen-class))
 
+(defn lcg [modulus multiplier increment]
+  (fn [seed]
+    (let [next-item (fn [previous-item] (mod (+ (* multiplier previous-item) increment) modulus))]
+      (iterate next-item seed))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/icfp2015/core_test.clj
+++ test/icfp2015/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [icfp2015.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest lcg-test
+  (testing "Correct sequence"
+    (let [f (lcg (bit-shift-left 1 32) 1103515245 12345)]
+      (is (= [17 1579902326 3232257911 794669028 2765327437] (take 5 (f 17)))))))
