2af0a61a9e49a41f76b4ad5e6eeec76e73cf8e8f
Chris LaRose <chris.larose@mycase.com>
2015-08-07 21:58:44 -0700

    cjl - Add stubs for board fns

--- src/icfp2015/core.clj
+++ src/icfp2015/core.clj
@@ -11,6 +11,30 @@
         truncate-bits (fn [x] (bit-and (bit-shift-right x 16) 0x7FFF))]
     (map truncate-bits (raw-seq seed))))
 
+; board state is represented as a current-unit together with its currently
+; filled-and-locked cells
+; e.g. { :current-unit { :members [2 0] [0 1] [2 2] :pivot [1 1] }
+;        :filled #{ [5 4] [4 3] } }
+(defn make-board
+  "Creates a new new board with an initial-unit"
+  [w h filled initial-unit]
+  nil)
+
+; these are possible commands
+; [ [:move :west] [:move :east] [:move :southwest] [:move :southeast]
+;   [:rotate :clockwise] [:rotate :counterclockwise] ]
+(defn transition-board
+  "Given a board and command, returns a new board. Raises if performing
+  the command puts the board into an illegal state"
+  [board command]
+  nil)
+
+(defn lock-and-spawn
+  "Locks the current unit. Clears rows. Moves cells down. Spawns new unit.
+  Raises if new-unit cannot be spawned"
+  [board new-unit]
+  nil)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
