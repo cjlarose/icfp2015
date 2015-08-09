8e7e46ca059d9dafcdbf3753bdaa451801dd15d8
Chris LaRose <chris.larose@mycase.com>
2015-08-09 15:54:33 -0700

    cjl - Using dumb AI to generate commands

--- src/icfp2015/core.clj
+++ src/icfp2015/core.clj
@@ -1,8 +1,11 @@
 (ns icfp2015.core
   (:require [clojure.data.json :as json]
             [clojure.tools.cli :refer [parse-opts]]
-            [icfp2015.problem :refer [get-games]])
+            [icfp2015.problem :refer [get-games]]
+            [icfp2015.gameplay :refer [get-commands]])
   (:gen-class))
+
+(def dumb-ai (constantly [:move :southeast]))
 
 (defn read-games-from-file [f]
   (-> f
@@ -10,10 +13,19 @@
       (json/read-str :key-fn keyword)
       (get-games)))
 
+(defn command-to-letter [command]
+  (case
+    [:move :west] \p
+    [:move :east] \b
+    [:move :southwest] \a
+    [:move :southeast] \l
+    [:rotate :clockwise] \d
+    [:rotate :counterclockwise] \k))
+
 (defn solve-game [game]
   { :problemId (:problemId game)
     :seed (:seed game)
-    :solution (apply str (repeat 3 "a")) })
+    :solution (map command-to-letter (get-commands dumb-ai game)) })
 
 (def cli-options
   [["-f" "--file FILE" "Input filename"
