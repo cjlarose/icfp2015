21febc257aeb98a0b0206ac43be330ce97553926
Chris LaRose <chris.larose@mycase.com>
2015-08-08 20:11:02 -0700

    cjl - Add beginning of gameplay. It's really ugly. Movement to the southeast seems incorrect

---
+++ src/icfp2015/gameplay.clj
@@ -0,0 +1,30 @@
+(ns icfp2015.gameplay
+  (:require [icfp2015.board :refer [make-board]]))
+
+; (defprotocol AI
+;   (get-move [board] "Returns a [new board, command] vector"))
+
+(defn handle-unit
+  [ai initial-board unit]
+  (let [f (fn [[board commands]]
+            ; (println board commands)
+            (let [[new-board new-command] (ai board)]
+              [new-board (conj commands new-command)]))]
+    (second (last (take-while
+      (fn [[{:keys [current-unit]} _]] current-unit)
+      (rest (iterate f [(assoc initial-board :current-unit unit) '()])))))))
+
+; (defn get-commands
+;   "Yields a seq of commands given a game and an AI"
+;   [{:keys [width height filled units]} ai]
+;   (let [f (fn [[board commands] unit] 
+;             (let [[new-board addtl-commands] (handle-unit board unit)]
+;               [new-board (concat commands addtl-commands)]))]
+;     (reduce f [(make-board width height filled) '()] units)))
+    
+;; make board
+;; for every unit
+;;   set current unit on board
+;;   until current-until is nil
+;;     feed board into ai 
+;;     append commands to list
---
+++ test/icfp2015/gameplay_test.clj
@@ -0,0 +1,22 @@
+(ns icfp2015.gameplay-test
+  (:require [clojure.test :refer :all]
+            [icfp2015.board :as board]
+            [icfp2015.gameplay :refer :all :as gameplay]))
+
+(deftest handle-unit-test
+  (testing "handles a single unit"
+    (let [board { :width 5
+                  :height 6
+                  :filled #{ }
+                  :current-unit nil }
+          unit { :members #{ [ 0 0 ] [ 0 1 ] }
+                 :pivot [ 0 0 ] }
+          cmd [:move :southeast]
+          mock-ai (fn [board]
+                    (let [new-board (board/transition-board board cmd)]
+                      (if (board/valid-position? new-board)
+                        [new-board cmd]
+                        [(board/transition-board board [:lock]) :lock])))
+          actual-commands (handle-unit mock-ai board unit)
+          expected-commands (repeat 3 cmd)]
+      (is (= expected-commands actual-commands)))))
