64b0c9e71b382c539e9183b55c12c87e47c42df3
Roey D. Chasman <rchasman@gmail.com>
2015-08-08 19:27:08 -0700

    RDC - Add some cell tests.

---
+++ test/icfp2015/cell_test.clj
@@ -0,0 +1,42 @@
+(ns icfp2015.cell-test
+  (:require [clojure.test :refer :all]
+            [icfp2015.cell :refer :all]))
+
+(deftest offset->cube-test
+  (testing "convert origin cell"
+    (let [cell [0 0]
+          actual-cell (offset->cube cell)
+          expected-cell [0 0 0]]
+      (is (= expected-cell actual-cell))))
+
+  (testing "convert [1 1] cell"
+    (let [cell [1 1]
+          actual-cell (offset->cube cell)
+          expected-cell [1 1 -2]]
+      (is (= expected-cell actual-cell))))
+
+  (testing "convert some cell"
+    (let [cell [4 -3]
+          actual-cell (offset->cube cell)
+          expected-cell [-5 4 1]]
+      (is (= expected-cell actual-cell)))))
+
+(deftest cube->offset-test
+  (testing "convert origin cell"
+    (let [cell [0 0 0]
+          actual-cell (cube->offset cell)
+          expected-cell [0 0]]
+      (is (= expected-cell actual-cell))))
+
+  (testing "convert [1 1 -2] cell"
+    (let [cell [1 1 -2]
+          actual-cell (cube->offset cell)
+          expected-cell [1 1]]
+      (is (= expected-cell actual-cell))))
+
+  (testing "convert some cell"
+    (let [cell [-1 5 -2]
+          actual-cell (cube->offset cell)
+          expected-cell [5 1]]
+      (is (= expected-cell actual-cell)))))
+
