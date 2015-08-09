b988b6802f10f44afea8a7de9725998c96b5eb4d
Chris LaRose <chris.larose@mycase.com>
2015-08-09 09:25:20 -0700

    cjl - More translation tests

--- test/icfp2015/unit_test.clj
+++ test/icfp2015/unit_test.clj
@@ -24,9 +24,23 @@
           expected-unit { :members #{ [1 0] [1 1] } :pivot [1 0] }]
       (is (= expected-unit actual-unit))))
 
+  (testing "translate odd row southwest"
+    (let [unit { :members #{ [1 1] [1 2] } :pivot [1 1] }
+          f (translate :southwest)
+          actual-unit (f unit)
+          expected-unit { :members #{ [2 1] [2 2 ] } :pivot [2 1] }]
+      (is (= expected-unit actual-unit))))
+
   (testing "translate even row southeast"
     (let [unit { :members #{ [0 0] [0 1] } :pivot [0 1] }
           f (translate :southeast)
           actual-unit (f unit)
           expected-unit { :members #{ [1 0] [1 1] } :pivot [1 1] }]
+      (is (= expected-unit actual-unit))))
+
+  (testing "translate odd row southeast"
+    (let [unit { :members #{ [1 0] [1 1] } :pivot [1 1] }
+          f (translate :southeast)
+          actual-unit (f unit)
+          expected-unit { :members #{ [2 1] [2 2] } :pivot [2 2] }]
       (is (= expected-unit actual-unit)))))
