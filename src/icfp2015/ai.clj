05c83ccd38fd37401148bba7b0108c90cb16b9f9
Chris LaRose <chris.larose@mycase.com>
2015-08-09 23:10:08 -0700

    cjl - Add slightly smarter ai

--- src/icfp2015/ai.clj
+++ src/icfp2015/ai.clj
@@ -1,3 +1,10 @@
-(ns icfp2015.ai)
+(ns icfp2015.ai
+  (:require [icfp2015.board :as board]))
 
 (def dumb-ai (constantly [:move :southeast]))
+
+(defn slightly-smarter-ai [board]
+  (let [new-board (board/transition-board board [:move :southwest])]
+    (if (nil? (:current-unit new-board))
+      [:move :southeast]
+      [:move :southwest])))
